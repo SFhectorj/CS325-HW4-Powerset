8491ac1ed2ceac2e1606cff260293668d9045e87
github_username <github_email>
2024-10-30 20:51:36 -0700

    powerset

--- PowerSet.py
+++ PowerSet.py
@@ -3,5 +3,13 @@ def powerset(inputSet):
     :param inputSet:
     :return:
     """
+    # This list will hold all subsets
     result = []
     return result
+
+def all_subsets(tracker, current_subset, inputSet, result):
+    # Base case: When the tracker reaches zero, all elements have been processed.
+    if tracker < 0:
+        # Copy the current subset to the final result.
+        result.append(current_subset[:])
+        return
