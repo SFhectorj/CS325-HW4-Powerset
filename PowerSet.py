33d0834cd1ab89486110f9a6cc9379cc600f8bd2
github_username <github_email>
2024-10-30 21:03:48 -0700

    powerset

--- PowerSet.py
+++ PowerSet.py
@@ -1,15 +1,16 @@
-def powerset(inputSet):
+def powerset(input_set):
     """
-    :param inputSet:
     :return:
     """
     # This list will hold all subsets
     result = []
     return result
 
-def all_subsets(tracker, current_subset, inputSet, result):
+
+def all_subsets(tracker, current_subset, input_set, result):
     # Base case: When the tracker reaches zero, all elements have been processed.
     if tracker < 0:
         # Copy the current subset to the final result.
+        # The copy must include all nested objects.
         result.append(current_subset[:])
         return
